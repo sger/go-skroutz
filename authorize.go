63109f40477f6d0e2f418413f065fd5d2be02ae9
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2017-01-20 21:45:51 +0200

    add authorize func

---
+++ authorize.go
@@ -0,0 +1,64 @@
+package skroutz
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strconv"
+)
+
+// AuthorizeResponse returns information about access token, token type
+type AuthorizeResponse struct {
+	AccessToken string  `json:"access_token"`
+	TokenType   string  `json:"token_type"`
+	Expires     float64 `json:"expires_in"`
+}
+
+// Authorize generate access_token
+func Authorize(clientID string, clientSecret string) (*AuthorizeResponse, error) {
+	apiURL := "https://www.skroutz.gr"
+	resource := "/oauth2/token/"
+	data := url.Values{}
+	data.Set("client_id", clientID)
+	data.Set("client_secret", clientSecret)
+	data.Set("grant_type", "client_credentials")
+	data.Set("scope", "public")
+
+	u, _ := url.ParseRequestURI(apiURL)
+	u.Path = resource
+	urlStr := fmt.Sprintf("%v", u)
+
+	client := &http.Client{}
+	r, _ := http.NewRequest("POST", urlStr, bytes.NewBufferString(data.Encode()))
+	r.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
+
+	resp, err := client.Do(r)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		defer resp.Body.Close()
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var result AuthorizeResponse
+	err = json.Unmarshal(body, &result)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &result, nil
+}
